cd425dedd6efca168ffb910a378b976531459e9a
doawelul <dfang314@gmail.com>
2025-07-15 13:08:50 -0700

    Move the rules skeletons to support html/css

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('public'))
+
 app.get('/', (req, res) => {
   res.send('Hello World! asdfasdf')
 })
@@ -10,10 +12,10 @@ app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
 
-app.get('/mtg', (req, res) => {
-  res.send('Here are simple rules for mtg')
-})
+// app.get('/mtg', (req, res) => {
+//   res.send('Here are simple rules for mtg')
+// })
 
-app.get('/dandan', (req, res) => {
-  res.send('Here are the rules for dandan')
-})
+// app.get('/dandan', (req, res) => {
+//   res.send('Here are the rules for dandan')
+// })
